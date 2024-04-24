727734176eae836ded2ee419bb4d5cb232e173b9
Fabian Klopfer <fabian.klopfer@ieee.org>
2024-04-24 14:14:31 +0200

    ddp training, better naming for checkpoints, adapt paths to server, remove finetuning schedule and retrain the whole model with smaller lr instead
    Signed-off-by: Fabian Klopfer <fabian.klopfer@ieee.org>

--- src/train.py
+++ src/train.py
@@ -15,24 +15,25 @@ from bssfp_to_dwi_tensor_model import (bSSFPToDWITensorModel, MultiInputUNet,
                                        TrainingState)
 
 
-def build_trainer_args(debug):
+def build_trainer_args(debug, modality, state):
     logger = pl.loggers.WandbLogger(project='dove',
                                     log_model='all',
                                     save_dir='logs')
     early_stopping_cb = pl.callbacks.EarlyStopping(monitor='val_loss',
                                                    patience=10)
-    swa_cb = pl.callbacks.StochasticWeightAveraging(swa_lrs=1e-2,
-                                                    device=None)
+    fname = f'{state}-{modality}-'
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
             save_top_k=10,
             monitor="val_loss",
             mode="min",
-            filename="{epoch:02d}-{val_loss:.4f}",
+            filename=fname + "{epoch:02d}-{val_loss:.4f}" + f'{datetime.datetime.now()}',
+            dirname="/home/fklopfer/logs/"
             )
-    cbs = [early_stopping_cb, swa_cb, checkpoint_callback]
+    cbs = [early_stopping_cb, checkpoint_callback]
     trainer_args = {'max_epochs': 100,
                     'accelerator': 'gpu',
-                    'devices': 1,
+                    'strategy': 'ddp',
+                    'devices': 'auto',
                     'precision': '32',
                     'accumulate_grad_batches': 1,
                     'logger': logger,
@@ -65,7 +66,7 @@ def train_model(net,
     start_total = start
 
     if 'all' in stages or 'pretrain' in stages:
-        trainer_args, ckpt_cb = build_trainer_args(debug)
+        trainer_args, ckpt_cb = build_trainer_args(debug, modality, TrainingState.PRETRAIN)
         trainer = pl.Trainer(**trainer_args)
 
         if ckpt_path:
@@ -86,24 +87,36 @@ def train_model(net,
         if debug:
             trainer_args['profiler'].plot()
             trainer_args['profiler'].summary()
+        ckpt_path = ckpt_cb.best_model_path
+
+        trainer = pl.Trainer(devices=1, num_nodes=1)
         trainer.test(model, datamodule=data)
 
-        ckpt_path = ckpt_cb.best_model_path
         wandb.finish()
 
+    if 'all' in stages or 'transfer' in stages:
+        assert ckpt_path, 'Need a checkpoint of a pretrained model to transfer'
+        trainer_args, ckpt_cb = build_trainer_args(debug, modality, TrainingState.TRANSFER)
+        trainer = pl.Trainer(**trainer_args)
+        model = bSSFPToDWITensorModel.load_from_checkpoint(ckpt_path, net=net)
+        model.change_training_state(TrainingState.TRANSFER, modality)
+
+        start = datetime.datetime.now()
+        print(f"Transfer for modality {modality} started at {start}")
+        trainer.fit(model, datamodule=data)
+        end = datetime.datetime.now()
+        print(f"Training finished at {end}.\nTook: {end - start}")
+        if debug:
+            trainer_args['profiler'].plot()
+            trainer_args['profiler'].summary()
+        ckpt_path = ckpt_cb.best_model_path
+
+        trainer = pl.Trainer(devices=1, num_nodes=1)
+        trainer.test(model, datamodule=data)
+        
     if 'all' in stages or 'finetune' in stages:
-        assert ckpt_path, 'Need a checkpoint to fine-tune from'
-        trainer_args, ckpt_cb = build_trainer_args(debug)
-        trainer_args['callbacks'] = [
-                FinetuningScheduler(ft_schedule=(
-                    'bSSFPToDWITensorModel_ft_schedule.yaml')),
-                FTSEarlyStopping(monitor='val_loss', patience=10),
-                FTSCheckpoint(monitor='val_loss', mode='min',
-                              filename='{epoch:02d}-{val_loss:.4f}',
-                              save_top_k=3),
-                pl.callbacks.StochasticWeightAveraging(swa_lrs=1e-2,
-                                                       device=None)
-                ]
+        assert ckpt_path, 'Need a checkpoint path of a transferred model to fine-tune'
+        trainer_args, ckpt_cb = build_trainer_args(debug, modality, TrainingState.FINE_TUNE)
         trainer = pl.Trainer(**trainer_args)
         model = bSSFPToDWITensorModel.load_from_checkpoint(ckpt_path, net=net)
         model.change_training_state(TrainingState.FINE_TUNE, modality)
@@ -116,24 +129,8 @@ def train_model(net,
         if debug:
             trainer_args['profiler'].plot()
             trainer_args['profiler'].summary()
-        trainer.test(model, datamodule=data)
 
-        wandb.finish()
-        ckpt_path = ckpt_cb.best_model_path
-
-        trainer_args, ckpt_cb = build_trainer_args(debug)
-        trainer = pl.Trainer(**trainer_args)
-        model = bSSFPToDWITensorModel.load_from_checkpoint(ckpt_path, net=net)
-        model.change_training_state(TrainingState.FINE_TUNE, modality)
-
-        start = datetime.datetime.now()
-        print(f"Fine tuning for modality {modality} started at {start}")
-        trainer.fit(model, datamodule=data)
-        end = datetime.datetime.now()
-        print(f"Training finished at {end}.\nTook: {end - start}")
-        if debug:
-            trainer_args['profiler'].plot()
-            trainer_args['profiler'].summary()
+        trainer = pl.Trainer(devices=1, num_nodes=1)
         trainer.test(model, datamodule=data)
 
     end = datetime.datetime.now()
@@ -153,17 +150,28 @@ if __name__ == "__main__":
     torch.set_float32_matmul_precision('medium')
     print(f'Last run on {time.ctime()}')
 
-    data = DoveDataModule('/home/someusername/workspace/DOVE/bids')
+    data = DoveDataModule('/ptmp/fklopfer/bids')
 
     unet = MultiInputUNet(TrainingState.PRETRAIN)
     print(unet)
     # check_input_shape(strides)
 
-    ckpt = train_model(unet, data, stages=['pretrain'])
-    # ckpt = ('/home/someusername/workspace/UNet-bSSFP/logs/dove/pretrain_v3/'
-    #        'checkpoints/epoch=43-val_loss=0.0063.ckpt')
-    # generate_default_finetuning_schedule(unet, data, 'dwi-tensor')
+    # ckpt = train_model(unet, data, stages=['pretrain'])
+    ckpts = [
+            ('/home/fklopfer/logs/finetune/dwi/'
+        'TrainingState.TRANSFER-dwi-tensor-epoch=14-val_loss=0.00712024-04-21 23:15:36.286439.ckpt'),
+            ('/home/fklopfer/logs/finetune/pc-bssfp/'
+                'TrainingState.TRANSFER-pc-bssfp-epoch=31-val_loss=0.04342024-04-22 14:24:15.094625.ckpt'),
+            ('/home/fklopfer/logs/finetune/one-bssfp/'
+                'TrainingState.TRANSFER-bssfp-epoch=31-val_loss=0.05482024-04-22 17:52:47.256176.ckpt')
+            ('/home/fklopfer/logs/finetune/t1w/epoch=54-val_loss=0.06342024-04-23 13:52:05.351665.ckpt')
+            ]
 
-    for modality in ['dwi-tensor', 'pc-bssfp', 'bssfp', 't1w']:
+    for ckpt in ckpts:
+        assert os.path.exists(ckpt), f'Typo in checkpoint path {ckpt}'
+
+    modalities = ['dwi-tensor', 'pc-bssfp', 'bssfp', 't1w']
+    # generate_default_finetuning_schedule(unet, data, 'dwi-tensor')
+    for modality, ckpt in zip(modalities, ckpts):
         train_model(unet, data, ckpt, modality, stages=['finetune'])
 

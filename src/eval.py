bdaaba6cb8e9155dafa53d25171a0432e8162379
Fabian Klopfer <fabian.klopfer@ieee.org>
2024-05-03 19:46:59 +0200

    relative error per segmentation
    Signed-off-by: Fabian Klopfer <fabian.klopfer@ieee.org>

--- src/eval.py
+++ src/eval.py
@@ -3,11 +3,14 @@ from multiprocessing import cpu_count, set_start_method
 import os
 import datetime
 from functools import partial
+from typing import Tuple
 import shutil
 
+from bids import BIDSLayout
+import lightning.pytorch as pl
 import nibabel as nib
 import numpy as np
-import lightning.pytorch as pl
+import pandas as pd
 import torch
 from tqdm import tqdm
 
@@ -149,7 +152,7 @@ def calc_scalar_maps(directory: str):
         if ('denorm' not in fname or ('_pred-' not in fname
                                     and '_target-' not in fname)
                 or '_ad' in fname or '_rd' in fname or '_fa' in fname
-                or '_md' in fname or 'azimuth' in fname 
+                or '_md' in fname or 'azimuth' in fname
                 or 'inclination' in fname or '_rgb' in fname):
             continue
         else:
@@ -215,21 +218,129 @@ def eval_model(unet, data, checkpoint_path, state,
         shutil.move(f_path, os.path.join(pred_dir, fname))
 
 
+# file path, mask, probsegs
+def do_calc_error_avg(args: Tuple[str, nib.Nifti1Image, nib.Nifti1Image]):
+    fname = args[0]
+    mask = args[1]
+    probseg = args[2]
+
+    modality = fname.split('_mod-')[-1].split('_2024-')[0]
+    pred_id = fname.split('_diff-')[-1].split('_state-')[0]
+    sub_id = fname.split('_sub-')[-1].split('_ses-')[0]
+    ses_id = fname.split('_ses-')[-1][0]
+    value_t = fname.split('_ses-')[-1][2:].split('.nii.gz')[0]
+    roi_names = ['CSF', 'GM', 'WM']
+
+    cols = ['modality', 'pred_id', 'sub', 'ses', 'roi']
+
+    if 'denorm' == value_t or 'nii.gz' == value_t:
+        suffix = '' if 'denorm' == value_t else '_norm'
+        cc = ['dxx', 'dxz', 'dxz', 'dyy', 'dyz', 'dzz']
+        cc = [c + suffix for c in cc]
+    else:
+        cc = [value_t]
+
+    rel_errors = pd.DataFrame(columns=cols)
+    rel_errors.set_index(['modality', 'pred_id', 'roi'], inplace=True)
+
+    orig = nib.load(fname)
+    diff_map = np.abs(orig.get_fdata())
+    diff_map = diff_map if len(diff_map.shape) == 4 else diff_map[..., np.newaxis]
+
+    for i in range(diff_map.shape[-1]):
+        for roi_idx in range(probseg.shape[-1]):
+            segmented = probseg[..., roi_idx] * diff_map[..., i]
+            norm = probseg[..., roi_idx].sum()
+            err = segmented.sum() / norm
+            vals = [modality, pred_id, sub_id, ses_id, roi_names[roi_idx]]
+            d = {col: val for col, val in zip(cols, vals)}
+            row = pd.DataFrame(data=d, index=[0])
+            row.set_index(['modality', 'pred_id', 'roi'], inplace=True)
+            row[cc[i]] = err
+            rel_errors = rel_errors.combine_first(row)
+
+    nib.save(nib.Nifti1Image(diff_map, orig.affine, orig.header), fname)
+    rel_errors.to_csv(fname.split('.nii.gz')[0] + '_rel_errors.csv')
+
+
+def calc_error_table(pred_path: str, data_path: str):
+    files = [os.path.join(pred_path, fn) for fn in next(os.walk(pred_path + 'dwi'))[2]]
+    subject_ids = list({f.split('_sub-')[-1].split('_ses-')[0] for f in files})
+
+    bids_layout = BIDSLayout(data_path, validate=False)
+    bids_layout.add_derivatives(data_path + '/derivatives/preproc-dove')
+
+    masks = {}
+    probsegs = {}
+    for sub in subject_ids:
+        mask_fname = bids_layout.get(scope='preproc-dove',
+                        subject=sub,
+                        extension='nii.gz',
+                        return_type='filename',
+                        desc='2mmiso',
+                        suffix='mask')[0]
+        masks[sub] = nib.load(mask_fname).get_fdata().astype(np.uint8)
+        probseg_fname = bids_layout.get(scope='preproc-dove',
+                        subject=sub,
+                        extension='nii.gz',
+                        return_type='filename',
+                        desc='probseg',
+                        suffix='T1w')[0]
+        probseg = nib.load(probseg_fname).get_fdata()
+        for i in range(probseg.shape[-1]):
+            probseg[..., i] = np.where(masks[sub] > 0, probseg[..., i], 0)
+            probseg[..., i] = np.where(probseg[..., i] > 1e-5, probseg[..., i], 0)
+        probsegs[sub] = probseg
+
+    argslist = []
+    for root, dnames, _ in os.walk(pred_path):
+        for dname in dnames:
+            dir_name = os.path.join(root, dname)
+            for fname in next(os.walk(dir_name))[2]:
+                if '_diff-' in fname and '.nii.gz' in fname:
+                    sub = fname.split('_sub-')[-1].split('_ses-')[0]
+                    f_p = os.path.join(dir_name, fname)
+                    argslist.append((f_p, masks[sub], probsegs[sub]))
+
+    #run_concurrently(do_calc_error_avg, argslist)
+
+    index_cols = ['modality', 'pred_id', 'roi']
+    rel_errors = pd.DataFrame(columns=['modality', 'pred_id', 'sub', 'ses', 'roi',
+        'dxx_norm', 'dxy_norm', 'dxz_norm', 'dyy_norm', 'dyz_norm',
+        'dzz_norm', 'dxx', 'dxz', 'dyy', 'dyz', 'dzz', 'md', 'fa',
+        'ad', 'rd', 'azimuth', 'incline'])
+    rel_errors.set_index(index_cols, inplace=True)
+    for root, dnames, _ in os.walk(pred_path):
+        for dname in dnames:
+            dir_name = os.path.join(root, dname)
+            for fname in next(os.walk(dir_name))[2]:
+                if 'rel_errors.csv' in fname:
+                    row = pd.read_csv(os.path.join(root, dname, fname))
+                    row.set_index(index_cols, inplace=True)
+                    print(row.columns)
+                    print(rel_errors.columns)
+                    print(row)
+                    print(rel_errors)
+                    rel_errors = rel_errors.combine_first(row)
+
+    print(rel_errors)
+    rel_errors.to_csv('relative_errors.csv')
+
+
 def eval_dwi_tensors(preds_dir, dwi_rescale_args_path):
     invert_dwi_tensor_norm(pred_dir, dwi_rescale_args_path)
     calc_scalar_maps(pred_dir)
     calc_diff_maps(pred_dir)
 
 
-if __name__ == "__main__":
-    set_start_method('spawn')
+def gen_predictions():
     torch.set_float32_matmul_precision('high')
 
     unet = MultiInputUNet(state=TrainingState.FINE_TUNE)
     data = DoveDataModule('/ptmp/fklopfer/bids')
     dwi_rescale_args_path = '/home/fklopfer/UNet-bSSFP/rescale_args_dwi.txt'
 
-    modalities = ['t1w'] # 'dwi-tensor', 'pc-bssfp', 'bssfp', 
+    modalities = ['t1w'] # 'dwi-tensor', 'pc-bssfp', 'bssfp',
     ckpts = [
 #            '/ptmp/fklopfer/logs/finetune/dwi/TrainingState.FINE_TUNE-dwi-tensor-epoch=00-val_loss=0.00662024-04-24 14:29:21.450677.ckpt',
 #            '/ptmp/fklopfer/logs/finetune/pc-bssfp-local-norm/TrainingState.FINE_TUNE-pc-bssfp-epoch=40-val_loss=0.03032024-04-24 17:39:30.603000.ckpt',
@@ -248,3 +359,7 @@ if __name__ == "__main__":
         eval_model(unet, data, ckpt, TrainingState.FINE_TUNE,
                    modality, pred_dir)
         eval_dwi_tensors(pred_dir, dwi_rescale_args_path)
+
+if __name__ == "__main__":
+    set_start_method('spawn')
+    calc_error_table('/ptmp/fklopfer/preds/finetune/best/', '/ptmp/fklopfer/bids')
